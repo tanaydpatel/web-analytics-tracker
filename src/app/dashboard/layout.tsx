9e1d1db9dc8432be26906ad208f07cd06abdb9cc
Tanay Patel <tanaydpatel@gmail.com>
2024-09-22 03:24:54 +0530

    ⚡ user profile sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -45,31 +45,46 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
         className="z-1 fixed left-0 top-0 h-screen w-64 -translate-x-full bg-bg-light-gray px-5 py-6 transition-transform sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="flex h-full flex-col overflow-y-auto">
-          <a href="https://www.tanaypatel.dev">
-            <img
-              src="/logo.svg"
-              className="me-3 h-6 sm:h-7"
-              alt="Surface Labs Logo"
-            />
-          </a>
-          <hr className="my-4 border-t border-gray-300" />
-          <button className="m-0 flex flex-row items-center justify-between px-4 text-base font-semibold text-text-black">
-            <div className="flex flex-row items-center">
-              <UserIcon className="h-4 w-4 text-text-black" />{" "}
-              <span className="ml-3 inline-block">My workspace</span>
+        <div className="flex h-full flex-col justify-between overflow-y-auto">
+          <div>
+            <a href="https://www.tanaypatel.dev">
+              <img
+                src="/logo.svg"
+                className="me-3 h-6 sm:h-7"
+                alt="Surface Labs Logo"
+              />
+            </a>
+            <hr className="my-4 border-t border-gray-300" />
+            <button className="m-0 flex flex-row items-center justify-between px-4 text-base font-semibold text-text-black">
+              <div className="flex flex-row items-center">
+                <UserIcon className="h-4 w-4 text-text-black" />{" "}
+                <span className="ml-3 inline-block">My workspace</span>
+              </div>
+              <ChevronDownIcon className="h-4 w-4 text-gray-500" />
+            </button>
+            <hr className="my-4 border-t border-gray-300" />
+            <button className="flex-column flex h-10 w-full items-center rounded-md bg-[#383f50] px-4">
+              <PlayCircleIcon className="h-5 w-5 text-white" />{" "}
+              <p className="ml-3 text-base text-white"> Getting Started</p>
+            </button>
+            <hr className="my-4 border-t border-gray-300" />
+            {sidebarOptions.map(({ icon, name }) => (
+              <SidebarOption icon={icon} name={name} key={name} />
+            ))}
+          </div>
+          <div className="flex items-center rounded-lg">
+            <div className="h-12 w-12 overflow-hidden rounded-[25px] bg-white bg-[url('https://www.tanaypatel.dev/profile.jpeg')] bg-cover bg-center bg-no-repeat" />
+            <div className="ml-3 flex w-full flex-1 items-center gap-3">
+              <div className="flex w-full flex-1 flex-col items-start">
+                <p className="text-base font-medium leading-6 tracking-[0.2px] text-[#383f50]">
+                  Tanay Patel
+                </p>
+                <small className="text-xs font-medium leading-4 text-[#6c7385]">
+                  me@tanaypatel.dev
+                </small>
+              </div>
             </div>
-            <ChevronDownIcon className="h-4 w-4 text-gray-500" />
-          </button>
-          <hr className="my-4 border-t border-gray-300" />
-          <button className="flex-column flex h-10 w-full items-center rounded-md bg-[#383f50] px-4">
-            <PlayCircleIcon className="h-5 w-5 text-white" />{" "}
-            <p className="ml-3 text-base text-white"> Getting Started</p>
-          </button>
-          <hr className="my-4 border-t border-gray-300" />
-          {sidebarOptions.map(({ icon, name }) => (
-            <SidebarOption icon={icon} name={name} key={name} />
-          ))}
+          </div>
         </div>
       </aside>
 
