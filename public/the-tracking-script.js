1ecc60bd7b9914faee588b5578ca154daca93040
Tanay Patel <tanaydpatel@gmail.com>
2024-09-22 23:52:09 +0530

    ⚡ identify user

--- public/the-tracking-script.js
+++ public/the-tracking-script.js
@@ -4,20 +4,40 @@
   script.src = "https://unpkg.com/analytics/dist/analytics.min.js";
   script.defer = true;
 
+  function getUserIdFromUrl() {
+    const scripts = document.getElementsByTagName("script");
+    console.log(scripts);
+    for (let i = 0; i < scripts.length; i++) {
+      const script = scripts[i];
+      if (script.src.includes("the-tracking-script")) {
+        const src = script.src;
+        const params = new URLSearchParams(src.split("?")[1]);
+        return params.get("id");
+      }
+    }
+    return null;
+  }
+
+  const accountId = getUserIdFromUrl();
+  console.log(accountId);
   script.onload = function () {
     if (typeof _analytics !== "undefined") {
       // Initialize analytics once the script is loaded
       const Analytics = _analytics.init({
         app: "the-super-tracking-app-by-tanay-patel",
-        version: 100,
+        version: 1,
         plugins: [],
       });
-
+      // create unique identifier for each user
+      const userId =
+        Analytics.storage.getItem("userId") || `${accountId}-${Date.now()}`;
+      Analytics.storage.setItem("userId", userId);
+      Analytics.identify(userId);
       // Track a page view event
-      Analytics.page();
-
-      // Optional: Initialize any default custom tracking
-      console.log("Analytics initialized");
+      Analytics.page({
+        title: document.title,
+        url: window.location.href,
+      });
     } else {
       console.error("Analytics library not found.");
     }
